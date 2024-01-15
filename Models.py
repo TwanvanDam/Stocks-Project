7f011434ec8d14e83ce7627f3624488462069c79
TwanvanDam <118354671+TwanvanDam@users.noreply.github.com>
2024-01-15 18:00:40 +0100

    Added Question C

---
+++ Models.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+class Vector_Model:
+    def __init__(self,mu, alpha, p):
+        self.mu = mu
+        self.alpha = alpha
+        self.p = p
+
+    def f(self, t, x):
+        return [self.mu*x[0], -(x[1]-x[2]), (x[1]-x[2])/self.alpha]
+
+    def g(self, t, x):
+        return [x[0]*x[1], self.p*x[1], 0]
+
+    def g_prime(self, t, x):
+        return [x[1],self.p,0]
+
+    def euler_method(self, x_0, W,t):
+        dt = t[1]-t[0] 
+        self.dt = dt
+        x = np.zeros((len(t),3))
+        x[0,:] = x_0
+
+        for time in range(len(t)-1):
+            for i in range(3):
+                x[time+1,i] = x[time,i] + self.f(time,x[time,:])[i]*dt + self.g(time,x[time,:])[i]*(W[time+1,i]-W[time,i])
+        return x[:,0]
+
+    def milstein_method(self, x_0, W,t):
+        dt = t[1]-t[0]
+        self.dt = dt
+        x = np.zeros((len(t),3))
+        x[0,:] = x_0
+
+        for time in range(len(t)-1):
+            for i in range(3):
+                x[time+1,i] = x[time,i] + self.f(time,x[time,:])[i]*dt + self.g(time,x[time,:])[i]*(W[time+1,i]-W[time,i]) + 0.5*self.g(time,x[time,:])[i]*self.g_prime(time,x[time,:])[i]*((W[time+1,i]-W[time,i])**2 - dt)
+        return x[:,0]
+
+class Black_Scholes:
+    def __init__(self, mu,sigma_0):
+        self.mu = mu
+        self.sigma_0 = sigma_0
+    
+    def f(self, t, x):
+        return self.mu*x
+    
+    def g(self, t, x):
+        return x*self.sigma_0
+    
+    def g_prime(self, t, x):
+        return self.sigma_0
+    
+    def euler_method(self, x_0, W,t):
+        dt = t[1]-t[0]
+        self.dt = dt
+        x = np.zeros(len(t))
+        x[0] = x_0
+
+        for time in range(len(t)-1):
+            x[time+1] = x[time] + self.f(time,x[time])*dt + self.g(time,x[time])*(W[time+1,0]-W[time,0])
+        return x
+    
+    def milstein_method(self, x_0, W,t):
+        dt = t[1]-t[0]
+        self.dt = dt
+        x = np.zeros(len(t))
+        x[0] = x_0
+
+        for time in range(len(t)-1):
+            x[time+1] = x[time] + self.f(time,x[time])*dt + self.g(time,x[time])*(W[time+1,0]-W[time,0]) + 0.5*self.g(time,x[time])*self.g_prime(time,x[time])*((W[time+1,0]-W[time,0])**2 - dt)
+        return x
---
+++ QuestionC.py
@@ -0,0 +1,94 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from Models import Black_Scholes, Vector_Model
+
+def Plot_different_timesteps(n_list, model, X_0, W, t_end=1, Euler=True):
+    dt_list = [t_end/n_list[i] for i in range(len(n_list))]
+    for size in n_list:
+            W_sample = W[::int(n_list[0]/size),:,0]
+            t = np.linspace(0, t_end, int(size))
+            
+            if Euler==True:
+                x = model.euler_method(X_0[0], W_sample,t)
+            else:
+                x = model.milstein_method(X_0[0], W_sample,t)
+            plt.plot(t,x,label=f"dt={t_end/size}")
+    plt.xlabel("t")
+    plt.ylabel("X")
+    plt.title("Different timesteps")
+    plt.grid()
+    plt.legend()
+    plt.savefig("./Plots/Different_timesteps.png",dpi=300)
+    plt.show()
+    return
+        
+
+
+def Strong_convergence(n_list, n_realisations, model, X_0, W, t_end=1):
+    dt_list = [t_end/n_list[i] for i in range(len(n_list))]
+
+    plot_list_euler = np.zeros((len(n)-1,n_realisations))
+    plot_list_milstein = np.zeros((len(n)-1,n_realisations))
+    
+    for j in range(n_realisations):
+        print(f"{j+1}/{n_realisations}")
+        for size in n_list:
+            W_sample = W[::int(n_list[0]/size),:,j]
+            t = np.linspace(0, t_end, int(size))
+            
+            x_euler = model.euler_method(X_0[0], W_sample,t)
+            x_milstein = model.milstein_method(X_0[0], W_sample,t)
+            #plt.plot(t,x_milstein)
+
+            if size == n_list[0]:
+                ground_truth_euler = x_euler
+                ground_truth_milstein = x_milstein
+            else:
+                error_euler = np.mean(np.abs(ground_truth_euler[::int(n[0]/size)] - x_euler))
+                plot_list_euler[n.index(size)-1,j] = error_euler
+
+                error_milstein = np.mean(np.abs(ground_truth_milstein[::int(n[0]/size)] - x_milstein))
+                plot_list_milstein[n.index(size)-1,j] = error_milstein
+    plt.loglog(dt_list[1:],np.mean(plot_list_euler,axis=1),'.-',label="euler")
+    plt.loglog(dt_list[1:],np.mean(plot_list_milstein,axis=1),'.-',label="milstein")
+    plt.xlabel("dt")
+    plt.ylabel("Error")
+    plt.title("Strong Convergence")
+    plt.grid()
+    plt.legend()
+    plt.savefig("./Plots/Strong_Convergence.png",dpi=300)
+    plt.show()
+
+
+
+if __name__ == "__main__":  
+    #Parameters
+    mu = 0.01
+    alpha = 0.5
+    p = 0
+
+    #Initial Conditions
+    sigma_0 = 0.2
+    xi_0 = 0.1
+    S_0 = 50
+    X_0 = [S_0,xi_0,sigma_0]
+
+    model = Black_Scholes(mu, sigma_0) #Vector_Model(mu, alpha, p)
+    #Time
+    order = 6
+    n = [int(10**i) for i in range(order,0,-1)]
+    dt_list = [1/n[i] for i in range(len(n))]
+    print("Using these timesteps for the strong convergence test:")
+    print(dt_list)
+    
+    t_end = 1
+    dt = dt_list[0]
+
+    np.random.seed(0)
+    n_realisations = 2
+    #ensure all methods use the same random numbers to ensure fair comparison
+    W = np.cumsum(np.random.normal(size=(int(n[0]),3,n_realisations),scale=math.sqrt(dt)),axis=0)
+
+    #Plot_different_timesteps(n, model, X_0, W, t_end=t_end, Euler=True)
+    Strong_convergence(n, n_realisations, model, X_0, W, t_end=t_end)
